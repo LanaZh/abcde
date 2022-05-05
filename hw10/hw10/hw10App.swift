e4680ed04406240f4c1d0c26c6636c51715c38d8
ZhukovaSveta <thelumierlana@yandex.ru>
2022-05-05 10:30:24 +0300

    add-color

---
+++ hw10/hw10/hw10App.swift
@@ -0,0 +1,17 @@
+//
+//  hw10App.swift
+//  hw10
+//
+//  Created by Надежда Жукова on 05.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct hw10App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
